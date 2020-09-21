9c11575a97831a58be1ebd51e5f0fca474dcb89c
Ryan Cahoon <ryan.m.cahoon@gmail.com>
2020-09-21 07:14:17 +0000

    Example code for running procedures asynchronously

---
+++ src/main/java/com/team766/frc2020/procedures/RaiseArm.java
@@ -0,0 +1,11 @@
+package com.team766.frc2020.procedures;
+
+import com.team766.framework.Procedure;
+import com.team766.framework.Context;
+
+public class RaiseArm extends Procedure {
+
+	public void run(Context context) {
+	}
+
+}
