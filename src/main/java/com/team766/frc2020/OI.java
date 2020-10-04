eadd5191b3372012e83a0bbba230aaf4f402a9aa
Ryan Cahoon <ryan.m.cahoon@gmail.com>
2020-10-04 19:09:29 +0000

    Tank drive tutorial code

--- src/main/java/com/team766/frc2020/OI.java
+++ src/main/java/com/team766/frc2020/OI.java
@@ -26,16 +26,11 @@ public class OI extends Procedure {
 	}
 	
 	public void run(Context context) {
+		context.takeOwnership(Robot.drive);
 		while (true) {
 			// Add driver controls here - make sure to take/release ownership
 			// of mechanisms when appropriate.
-			log("J0 A0: " + m_joystick0.getAxis(0) +
-			    "  J0 A1: " + m_joystick0.getAxis(1) +
-			    "  J1 A0: " + m_joystick1.getAxis(0) +
-			    "  J1 A1: " + m_joystick1.getAxis(1) +
-			    "  J0 B1: " + m_joystick0.getButton(1) +
-			    "  J0 B2: " + m_joystick0.getButton(2) +
-			    "  J0 B3: " + m_joystick0.getButton(3));
+			Robot.drive.setDrivePower(m_joystick0.getAxis(1), m_joystick1.getAxis(1));
 
 			context.waitFor(() -> RobotProvider.instance.hasNewDriverStationData());
 		}
