b545e5dec5f774d968392ee17e1339843e55e791
Ryan Cahoon <ryan.m.cahoon@gmail.com>
2020-10-29 09:51:53 +0000

    Drive encoders and DriveDistance

--- src/main/java/com/team766/frc2020/mechanisms/Drive.java
+++ src/main/java/com/team766/frc2020/mechanisms/Drive.java
@@ -1,16 +1,24 @@
 package com.team766.frc2020.mechanisms;
 
 import com.team766.framework.Mechanism;
+import com.team766.hal.EncoderReader;
 import com.team766.hal.RobotProvider;
 import com.team766.hal.SpeedController;
+import com.team766.logging.Category;
 
 public class Drive extends Mechanism {
 	private SpeedController m_leftMotor;
 	private SpeedController m_rightMotor;
+	private EncoderReader m_leftEncoder;
+	private EncoderReader m_rightEncoder;
 
 	public Drive() {
+		loggerCategory = Category.DRIVE;
+
 		m_leftMotor = RobotProvider.instance.getMotor("drive.leftMotor");
 		m_rightMotor = RobotProvider.instance.getMotor("drive.rightMotor");
+		m_leftEncoder = RobotProvider.instance.getEncoder("drive.leftEncoder");
+		m_rightEncoder = RobotProvider.instance.getEncoder("drive.rightEncoder");
 	}
 
 	public void setDrivePower(double leftPower, double rightPower) {
@@ -24,5 +32,19 @@ public class Drive extends Mechanism {
 		double leftMotorPower = turn + forward;
 		double rightMotorPower = -turn + forward;
 		setDrivePower(leftMotorPower, rightMotorPower);
+	}
+
+	public double getEnoderDistance() {
+		double leftValue = m_leftEncoder.getDistance();
+		double rightValue = m_rightEncoder.getDistance();
+		log("Encoders: " + leftValue + " " + rightValue);
+		return (leftValue + rightValue) / 2;
+	}
+
+	public void resetEncoders() {
+		checkContextOwnership();
+
+		m_leftEncoder.reset();
+		m_rightEncoder.reset();
 	}
 }
